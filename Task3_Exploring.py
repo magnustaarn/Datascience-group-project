c681960814a6a1ecabb875791e7104224e291425
McKlunk <mads716p@gmail.com>
2026-03-12 13:21:53 +0100

    Update Task3_Exploring.py

--- Task3_Exploring.py
+++ Task3_Exploring.py
@@ -3,13 +3,13 @@ from collections import Counter
 import matplotlib.pyplot as plt
 import Clean as clean
 
-df = pd.read_csv("processed_data.csv")
+df = pd.read_csv("995,000_rows.csv")
 
 df = clean.data_pipeline(df)
 
 dictionary_tokenized = clean.build_dictionary(df["token_without_stopwords"])
 dictionary_stemmed = clean.build_dictionary(df["stemmed_text"])
-dictionary_non_tokenized = counter(
+dictionary_non_tokenized = Counter(
   word for row in df["cleaned_text"] for word in row.split()
 )
 
@@ -19,13 +19,22 @@ date_count = dictionary_non_tokenized["DATE_TOKEN"]
 num_count = dictionary_non_tokenized["NUM_TOKEN"]
 
 #Frequency of words
-dictionary_tokenized.most_common(100)
 
 top100words = dictionary_tokenized.most_common(100)
 for word, count in top100words:
   print(word, count)
 
 #Plot
-clean.plot_most_frequent_words_from_dict(dictionary_tokenized)
+clean.plot_most_frequent_words_from_dict(dictionary_tokenized, 100)
 
 clean.plot_most_frequent_words(dictionary_tokenized)
+
+#Print dictionaries
+print("URL count:", url_count)
+print("Date count:", date_count)
+print("Number count:", num_count)
+
+#Before and after
+clean.plot_most_frequent_words_from_dict(dictionary_non_tokenized, 100)
+clean.plot_most_frequent_words_from_dict(dictionary_stemmed, 100)
+clean.plot_most_frequent_words_from_dict(dictionary_tokenized, 100)
