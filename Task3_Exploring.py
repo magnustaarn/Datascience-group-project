3c9a8be57b1065853ad4ffdda9d9bd91e05ea218
McKlunk <mads716p@gmail.com>
2026-03-12 12:47:48 +0100

    Update Task3_Exploring.py

--- Task3_Exploring.py
+++ Task3_Exploring.py
@@ -3,6 +3,9 @@ from collections import Counter
 import matplotlib.pyplot as plt
 import Clean as clean
 
+#Counting
 url_count = dictionary_non_tokenized["URL_TOKEN"]
 date_count = dictionary_non_tokenized["DATE_TOKEN"]
 num_count = dictionary_non_tokenized["NUM_TOKEN"]
+
+#Frequency of words
