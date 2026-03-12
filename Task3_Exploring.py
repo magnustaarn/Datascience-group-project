e018d4d475e8e911e35e5c7dfed51a4bc9612871
McKlunk <mads716p@gmail.com>
2026-03-12 13:05:31 +0100

    Update Task3_Exploring.py

--- Task3_Exploring.py
+++ Task3_Exploring.py
@@ -3,6 +3,16 @@ from collections import Counter
 import matplotlib.pyplot as plt
 import Clean as clean
 
+df = pd.read_csv("995,000_rows.csv")
+
+df = clean.data_pipeline(df)
+
+dictionary_tokenized = clean.build_dictionary(df["token_without_stopwords"])
+dictionary_stemmed = clean.build_dictionary(df["stemmed_text"])
+dictionary_non_tokenized = counter(
+  word for row in df["cleaned_text"] for word in row.split()
+)
+
 #Counting
 url_count = dictionary_non_tokenized["URL_TOKEN"]
 date_count = dictionary_non_tokenized["DATE_TOKEN"]
