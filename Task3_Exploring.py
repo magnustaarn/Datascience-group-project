50087aabfca22abd8ac703a8b2d9b597be754649
McKlunk <mads716p@gmail.com>
2026-03-12 12:45:45 +0100

    Update Task3_Exploring.py

--- Task3_Exploring.py
+++ Task3_Exploring.py
@@ -1,1 +1,8 @@
+import pandas as pd
+from collections import Counter
+import matplotlib.pyplot as plt
+import Clean as clean
 
+url_count = dictionary_non_tokenized["URL_TOKEN"]
+date_count = dictionary_non_tokenized["DATE_TOKEN"]
+num_count = dictionary_non_tokenized["NUM_TOKEN"]
