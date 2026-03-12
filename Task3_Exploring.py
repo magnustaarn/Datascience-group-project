7d9c59dadb943aaf840af237e9415ad732fc0188
McKlunk <mads716p@gmail.com>
2026-03-12 12:57:47 +0100

    Update Task3_Exploring.py

--- Task3_Exploring.py
+++ Task3_Exploring.py
@@ -9,3 +9,10 @@ date_count = dictionary_non_tokenized["DATE_TOKEN"]
 num_count = dictionary_non_tokenized["NUM_TOKEN"]
 
 #Frequency of words
+dictionary_tokenized.most_common(100)
+
+top100words = dictionary_tokenized.most_common(100)
+for word, count in top100words:
+  print(word, count)
+
+clean.plot_most_frequent_words(dictionary_tokenized)
