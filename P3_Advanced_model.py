076e6f01bc59fd612d87da3aace64b790d909613
Mads Pedersen <mads716p@gmail.com>
2026-03-26 11:09:49 +0100

    Improved Advanced P3_Advanced_model

--- P3_Advanced_model.py
+++ P3_Advanced_model.py
@@ -3,50 +3,54 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.svm import LinearSVC
 from sklearn.metrics import f1_score, classification_report
 
-#Load stopwords
-stopword_df = pd.read_csv("englishST.txt", header=None, names=["stopword"])
-custom_stopwords = set(stopwords_df["stopword"])
-
-
 #Load train/val/test
-df_train = pd.read_csv("train.csv")
-df_val = pd.read_csv("val.csv")
-df_test = pd.read_csv("test.csv")
+print("Loading data")
+df_train = pd.read_csv("train.csv", usecols=["stemmed_text", "label"])
+df_val = pd.read_csv("validation.csv", usecols=["stemmed_text", "label"])
+df_test = pd.read_csv("test.csv", usecols=["stemmed_text", "label"])
+
 
 #Remove NaN
-df_train = df_train.dropna(subset=["content","label"])
-df_val = df_val.dropna(subset=["content","label"])
-df_test = df_test.dropna(subset=["content","label"])
+print("Removing NaN")
+df_train = df_train.dropna(subset=["stemmed_text", "label"])
+df_val = df_val.dropna(subset=["stemmed_text", "label"])
+df_test = df_test.dropna(subset=["stemmed_text", "label"])
 
 #TF-IDF Vectorizer
+print("Starting TF-IDF")
 vectorizer = TfidfVectorizer(
   max_features = 20000, #Eller 10000?
   ngram_range = (1,2),
   min_df = 5,
   max_df = 0.8,
-  stop_words = custom_stopwords
 )
 
 #Fit and transform
-X_train = vectorizer.fit_transform(df_train["content"])
-X_val = vectorizer.transform(df_val["content"])
-X_test = vectorizer.transform(df_test["content"])       
+print("Starting vectorizer...")
+X_train = vectorizer.fit_transform(df_train["stemmed_text"])
+X_val = vectorizer.transform(df_val["stemmed_text"])
+X_test = vectorizer.transform(df_test["stemmed_text"])
+print("Vectorizer finished")      
 
 y_train = df_train["label"]
 y_val = df_val["label"]
 y_test = df_test["label"]
 
 #SVM Model
+print("Training model...")
 model = LinearSVC(class_weight="balanced", max_iter=10000)
 model.fit(X_train, y_train)
+print("Model finished")
 
 #Evaluation
+print("\nValidation:")
 y_val_pred = model.predict(X_val)
-print("Val F1 score:", f1_score(y_val, y_val_pred))
+print("F1 score:", f1_score(y_val, y_val_pred))
 print(classification_report(y_val, y_val_pred))
 
+print("\nTest:")
 y_test_pred = model.predict(X_test)
-print("Test F1 score:", f1_score(y_test, y_test_pred))
+print("F1 score:", f1_score(y_test, y_test_pred))
 print(classification_report(y_test, y_test_pred))
 
                   
